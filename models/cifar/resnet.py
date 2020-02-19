fb710ddc4387c8a5e7c65c79b6fdb2d0e69ab3e3
Zaid Khan <brain@zaidkhan.me>
2020-02-19 16:27:02 -0500

    black: Formatted code.

--- models/cifar/resnet.py
+++ models/cifar/resnet.py
@@ -1,22 +1,24 @@
 from __future__ import absolute_import
 
-'''Resnet for cifar dataset.
+"""Resnet for cifar dataset.
 Ported form
 https://github.com/facebook/fb.resnet.torch
 and
 https://github.com/pytorch/vision/blob/master/torchvision/models/resnet.py
 (c) YANG, Wei
-'''
+"""
 import torch.nn as nn
 import math
 
 
-__all__ = ['resnet']
+__all__ = ["resnet"]
+
 
 def conv3x3(in_planes, out_planes, stride=1):
     "3x3 convolution with padding"
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-                     padding=1, bias=False)
+    return nn.Conv2d(
+        in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False
+    )
 
 
 class BasicBlock(nn.Module):
@@ -58,8 +60,9 @@ class Bottleneck(nn.Module):
         super(Bottleneck, self).__init__()
         self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
-                               padding=1, bias=False)
+        self.conv2 = nn.Conv2d(
+            planes, planes, kernel_size=3, stride=stride, padding=1, bias=False
+        )
         self.bn2 = nn.BatchNorm2d(planes)
         self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
         self.bn3 = nn.BatchNorm2d(planes * 4)
@@ -91,18 +94,16 @@ class Bottleneck(nn.Module):
 
 
 class ResNet(nn.Module):
-
     def __init__(self, depth, num_classes=1000):
         super(ResNet, self).__init__()
         # Model type specifies number of layers for CIFAR-10 model
-        assert (depth - 2) % 6 == 0, 'depth should be 6n+2'
+        assert (depth - 2) % 6 == 0, "depth should be 6n+2"
         n = (depth - 2) // 6
 
-        block = Bottleneck if depth >=44 else BasicBlock
+        block = Bottleneck if depth >= 44 else BasicBlock
 
         self.inplanes = 16
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, padding=1,
-                               bias=False)
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(16)
         self.relu = nn.ReLU(inplace=True)
         self.layer1 = self._make_layer(block, 16, n)
@@ -114,7 +115,7 @@ class ResNet(nn.Module):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2. / n))
+                m.weight.data.normal_(0, math.sqrt(2.0 / n))
             elif isinstance(m, nn.BatchNorm2d):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
@@ -123,8 +124,13 @@ class ResNet(nn.Module):
         downsample = None
         if stride != 1 or self.inplanes != planes * block.expansion:
             downsample = nn.Sequential(
-                nn.Conv2d(self.inplanes, planes * block.expansion,
-                          kernel_size=1, stride=stride, bias=False),
+                nn.Conv2d(
+                    self.inplanes,
+                    planes * block.expansion,
+                    kernel_size=1,
+                    stride=stride,
+                    bias=False,
+                ),
                 nn.BatchNorm2d(planes * block.expansion),
             )
 
@@ -139,7 +145,7 @@ class ResNet(nn.Module):
     def forward(self, x):
         x = self.conv1(x)
         x = self.bn1(x)
-        x = self.relu(x)    # 32x32
+        x = self.relu(x)  # 32x32
 
         x = self.layer1(x)  # 32x32
         x = self.layer2(x)  # 16x16
--- models/cifar/vgg.py
+++ models/cifar/vgg.py
@@ -3,21 +3,29 @@ import torch.utils.model_zoo as model_zoo
 import math
 
 
-__all__ = [
-    'VGG', 'vgg16', 'vgg16_bn'
-]
+__all__ = ["VGG", "vgg16", "vgg16_bn"]
+
 
 class VGG(nn.Module):
-
-    def __init__(self, features, num_classes=1000, input_size = 32):
+    def __init__(self, features, num_classes=1000, input_size=32):
         super(VGG, self).__init__()
         self.features = features
         if input_size == 32:
-            self.classifier = nn.Sequential(nn.Linear(512,512), nn.ReLU(inplace=True), \
-                                        nn.BatchNorm1d(512),nn.Dropout2d(0.5),nn.Linear(512, num_classes))
+            self.classifier = nn.Sequential(
+                nn.Linear(512, 512),
+                nn.ReLU(inplace=True),
+                nn.BatchNorm1d(512),
+                nn.Dropout2d(0.5),
+                nn.Linear(512, num_classes),
+            )
         elif input_size == 64:
-            self.classifier = nn.Sequential(nn.Linear(2048,512), nn.ReLU(inplace=True), \
-                                        nn.BatchNorm1d(512),nn.Dropout2d(0.5),nn.Linear(512, num_classes))
+            self.classifier = nn.Sequential(
+                nn.Linear(2048, 512),
+                nn.ReLU(inplace=True),
+                nn.BatchNorm1d(512),
+                nn.Dropout2d(0.5),
+                nn.Linear(512, num_classes),
+            )
         self._initialize_weights()
 
     def forward(self, x):
@@ -30,7 +38,7 @@ class VGG(nn.Module):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2. / n))
+                m.weight.data.normal_(0, math.sqrt(2.0 / n))
                 if m.bias is not None:
                     m.bias.data.zero_()
             elif isinstance(m, nn.BatchNorm2d):
@@ -46,9 +54,9 @@ def make_layers(cfg, batch_norm=False):
     layers = []
     in_channels = 3
     for v in cfg:
-        if v == 'M':
+        if v == "M":
             layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
-        elif type(v)==int:
+        elif type(v) == int:
             conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
             if batch_norm:
                 layers += [conv2d, nn.ReLU(inplace=True), nn.BatchNorm2d(v)]
@@ -56,14 +64,43 @@ def make_layers(cfg, batch_norm=False):
             else:
                 layers += [conv2d, nn.ReLU(inplace=True)]
             in_channels = v
-        elif type(v)==float:
+        elif type(v) == float:
             layers += [nn.Dropout2d(v)]
     return nn.Sequential(*layers)
 
 
 cfg = {
-    'D': [64,0.3, 64, 'M', 128,0.4, 128, 'M', 256,0.4, 256,0.4, 256, 'M', 512,0.4, 512,0.4, 512, 'M', 512,0.4, 512,0.4, 512, 'M',0.5]
+    "D": [
+        64,
+        0.3,
+        64,
+        "M",
+        128,
+        0.4,
+        128,
+        "M",
+        256,
+        0.4,
+        256,
+        0.4,
+        256,
+        "M",
+        512,
+        0.4,
+        512,
+        0.4,
+        512,
+        "M",
+        512,
+        0.4,
+        512,
+        0.4,
+        512,
+        "M",
+        0.5,
+    ]
 }
+
 
 def vgg16(**kwargs):
     """VGG 16-layer model (configuration "D")
@@ -71,12 +108,11 @@ def vgg16(**kwargs):
     Args:
         pretrained (bool): If True, returns a model pre-trained on ImageNet
     """
-    model = VGG(make_layers(cfg['D']), **kwargs)
+    model = VGG(make_layers(cfg["D"]), **kwargs)
     return model
 
 
 def vgg16_bn(**kwargs):
     """VGG 16-layer model (configuration "D") with batch normalization"""
-    model = VGG(make_layers(cfg['D'], batch_norm=True), **kwargs)
+    model = VGG(make_layers(cfg["D"], batch_norm=True), **kwargs)
     return model
-
