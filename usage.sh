efa8e07ad9d246a31f608e54af3f4bcbefade701
Zaid Khan <brain@zaidkhan.me>
2020-02-19 16:40:04 -0500

    Removed commented-out lines.

--- usage.sh
+++ usage.sh
@@ -3,8 +3,5 @@
 # Training.
 rewards=(2.2)
 coverage=(0.80)
-dataset=cifar10
-#python3 main.py --rewards ${rewards[@]} --dataset $dataset --fastdebug
-python3 main.py --rewards ${rewards[@]} --dataset $dataset --fastdebug --train --resume saved_checkpoints/cifar10/vgg16_bn_gambling_o=2.20/300.pth
-
-#python3 main.py --rewards ${rewards[@]}  --evaluate --coverage ${coverage[@]}
+dataset=svhn
+python3 main.py --rewards ${rewards[@]} --dataset $dataset --fastdebug
