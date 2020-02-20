5e090129cf5888cc39753c46a8e98da2202256d0
Zaid Khan <brain@zaidkhan.me>
2020-02-20 01:04:06 -0500

    feat: Added branch for training.

--- usage.sh
+++ usage.sh
@@ -5,4 +5,6 @@ rewards=(2.2)
 coverage=(0.80)
 dataset=cifar10
 gpu_id=1
-python3 main.py --rewards "${rewards[@]}" --dataset $dataset --gpu-id $gpu_id
+trained_model=saved_checkpoints/cifar10/vgg16_bn_gambling_o\=2.20/300.pth
+#python3 main.py --rewards "${rewards[@]}" --dataset $dataset --gpu-id $gpu_id
+python3 main.py --rewards "${rewards[@]}" --dataset $dataset --gpu-id $gpu_id --train --resume $trained_model
